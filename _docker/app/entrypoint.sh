92a8d9b3bef5b4efdc36dd11dbc3bb59c9557147
Artem Kalinichenko <artem21kalinichenko@gmail.com>
2026-06-12 17:57:43 +0300

    changes in docker entrypoint

--- _docker/app/entrypoint.sh
+++ _docker/app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-su -s /bin/sh www-data -c "[ -f .env ] || cp .env.example .env"
+# su -s /bin/sh www-data -c "[ -f .env ] || cp .env.example .env"
 su -s /bin/sh www-data -c "composer install --no-interaction --prefer-dist --optimize-autoloader"
 su -s /bin/sh www-data -c "grep -q '^APP_KEY=.\+' .env || php artisan key:generate --force"
 su -s /bin/sh www-data -c "php artisan migrate --force"
