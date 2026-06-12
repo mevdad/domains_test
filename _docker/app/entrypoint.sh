d420133928e8fb4fbfbaf224fb03090200f9c084
Artem Kalinichenko <artem21kalinichenko@gmail.com>
2026-06-12 16:47:56 +0300

    changes in docker entrypoint

--- _docker/app/entrypoint.sh
+++ _docker/app/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+su -s /bin/sh www-data -c "composer install --no-interaction --prefer-dist --optimize-autoloader"
 su -s /bin/sh www-data -c "php artisan migrate --force"
 
 exec "$@"
